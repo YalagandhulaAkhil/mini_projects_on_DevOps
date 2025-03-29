508ce1df1fb4d3633742077d26c1e01aeaf252c3
YalagandhulaAkhil <131948225+YalagandhulaAkhil@users.noreply.github.com>
2025-03-29 16:14:04 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -74,7 +74,7 @@ resource "aws_security_group" "sg" {
   ingress {
     from_port   = 8000
     to_port     = 8000
-    protocol    = "HTTPS"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
